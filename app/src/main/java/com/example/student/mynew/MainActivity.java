ebd6c8e6eb126c5240ce9c030a36f013b0273c0b
798817228 <798817228@qq.com>
2017-11-02 18:46:06 +0800

    a

---
+++ app/src/main/java/com/example/student/mynew/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.student.mynew;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hand=new Handler();
+        view=(MyView)findViewById(R.id.myview);
+        hand.postDelayed(run,100);
+    }
+
+    Handler hand;
+    MyView view;
+    Runnable run=new Runnable() {
+        @Override
+        public void run() {
+            view.invalidate();
+            hand.postDelayed(run,100);
+        }
+    };
+
+
+}
